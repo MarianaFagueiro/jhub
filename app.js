74b7d0ba6ac9f1de564e716f9ce9d71c6d0b2aea
marianafagueiro <mariana.fagueiro@gmail.com>
2024-05-09 01:08:18 -0300

    desenvolvendo a página inicial e a página de preferencia com responsividade

--- app.js
+++ app.js
@@ -5,25 +5,30 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/index', (req, res) => { 
-    res.render('index');
-});
-
 app.get('/', (req, res) => {
     res.redirect('/index');
 });
 
-app.get('/preferencias', (req, res) => {
+app.get('/index', (req, res) => { 
+    res.render('index');
+});
+
+app.get('/preferencias', (req, res) => { 
     res.render('preferencias');
 });
 
-app.get('/feed', (req, res) => {
+app.get('/feed', (req, res) => { 
     res.render('feed');
+});
+
+app.get('/test', (req, res) => {
+    console.log('Servidor iniciado.');
 });
 
 app.listen(9000, () => {
     console.log('Servidor iniciado.');
 });
+
 
 
 
